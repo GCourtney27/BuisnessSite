560c62ba1d512797fdc5dc86d5dde56777d06d0a
gcourtney27 <gcourtney@student.neumont.edu>
2019-11-11 12:45:59 -0700

    Added: menu navigation between Home, About, and Admin pages; Page class for data parsing later; Page manager and Factory.

---
+++ PHP_Includes/menu.php
@@ -0,0 +1,21 @@
+<?php
+// ============================
+// Serves as the menu bar system for all pages.
+// To add a new page type in page title and link to pave in the key => value box
+// ============================
+$menuArr = [
+    // Menu item name => link to another file
+    "Home | " => "../Pages/index.php",
+    "About | " => "../Pages/about.php",
+    "Admin (DEBUG)" => "../Pages/admin.php"
+];
+
+echo "<div class='menu'>";
+foreach($menuArr as $key => $value){
+    echo " <a lass='button' href=$value>
+            $key
+            </a>";
+}
+echo "</div>";
+
+?>

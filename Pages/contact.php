274c41ad67ac80a52e25728bdb6f8bd61b4ff4ea
Ash Snell <ashleysnell219@yahoo.com>
2019-11-19 14:32:02 -0700

    Style two
    added the second style page

--- Pages/contact.php
+++ Pages/contact.php
@@ -13,7 +13,7 @@
 <div class="mainContent">
         <h1><?php include "../PHP_Includes/header.php"?></h1>
         <div class="menu"><?php include "../PHP_Includes/menu.php"?></div>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 
 
        <?php 
@@ -23,7 +23,7 @@
         ];
 
         foreach($menuArr as $key => $value){
-            echo " <a lass='button' href=$value> $key </a>";
+            echo " <a class='button' href=$value> $key </a>";
         }
 ?>
 

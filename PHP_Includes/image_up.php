78a87bd4fbe095c577bfeaf062dd87ab97270a57
Mooba <mombasa3d@gmail.com>
2019-11-19 18:53:50 -0700

    Fixed Image uploading
    - Still needs pointers to DB if using
    - merged about_misson and about_history with my versions

--- PHP_Includes/image_up.php
+++ PHP_Includes/image_up.php
@@ -1,31 +1,29 @@
 <?php
    if(isset($_FILES['image'])){
+
     $errors= array();
     $file_name = $_FILES['image']['name'];
-    $file_size = $_FILES['image']['size'];
     $file_tmp = $_FILES['image']['tmp_name'];
     $file_type = $_FILES['image']['type'];
     $extensions= array("jpeg","jpg","png");
-    
-    if(empty($errors)==true) {
-       move_uploaded_file($file_tmp, dirname(getcwd(), 1)."/Images/".$file_name);
-       echo dirname(getcwd(), 2);
-       echo "Success";
-    }else{
-       print_r($errors);
+   
+    if(empty($errors)==true){
+       move_uploaded_file($file_tmp, dirname(getcwd(), 1)."\\Images\\".$file_name);
+       $page_image = $file_name;
     }
- }
+}
 ?>
-
-
  
- <?php if(!isset($_FILES['image]'])): ?>
-<!-- <input type="file" accept="image" type="submit">Upload Image</input> -->
-<form action="" method="post" enctype="multipart/form-data">
+ <?php if(!isset($page_image)): ?>
+
+<form action "image_up.php" method="post" enctype="multipart/form-data">
     Select image:
     <br><input type="file" name="image"/>
     <input type="submit">
 </form>
+
 <?php else: ?>
-<img src="<?php echo $_GET.PHP; ?>" />;
-<?php endif ?> 
+
+<img src="<?php echo "..\\Images\\".$page_image?>" />
+<?php echo dirname(getcwd(),1)."\\Images\\".$page_image?>
+<?php endif; ?> 
